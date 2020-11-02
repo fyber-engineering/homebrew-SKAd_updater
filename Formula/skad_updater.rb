d86b727e2bf43e8e2dc993e27bc9b5572b2235d4
Daniel Krupitsky <34886222+dkwi@users.noreply.github.com>
2020-11-02 17:30:33 +0100

    Update to version 1.0.3

--- Formula/skad_updater.rb
+++ Formula/skad_updater.rb
@@ -1,9 +1,9 @@
 class SkadUpdater < Formula
-  desc "SK Ad Networks Updater"
-  homepage "https://github.com/fyber-engineering/SKAd_updater"
-  url "https://github.com/fyber-engineering/SKAd_updater/releases/download/1.0.0/skad_updater-1.0.0.tar.gz"
-  sha256 "ffb29620f5550df2dcc0599e3b54d25850cdcf45c2016056c3d2e97526a632a9"
-  license ""
+  desc "SKAdNetworks Updater"
+  homepage "https://github.com/fyber-engineering/SKAd_updater/"
+  url "https://github.com/fyber-engineering/SKAd_updater/releases/download/1.0.3/skad_updater-1.0.3.tar.gz"
+  sha256 "c46d403bb3f737196f73efa737a63a4ceb5cc956dd07fde40f0ab9dfc117baf6"
+  license "MIT"
 
   bottle :unneeded
 
@@ -17,7 +17,7 @@ class SkadUpdater < Formula
     puts "Testing SKAD updater"
     help_out =  shell_output("skad_updater -h")
     puts "skad_updater -h" + help_out
-    assert_true help_out.start_with?("*** Welcome to SK Ad Updater")
+    assert_true help_out.start_with?("*** Welcome to SKAd Updater")
     puts  shell_output("man skad_updater")
     assert_true shell_output("man skad_updater")
   end
